c320ae8cd563bc440b0c834b8ea3f8b848530780
Ephraim <ephraim.aigbefo@outlook.com>
2019-05-30 02:36:21 +0100

    Updates

---
+++ precache-manifest.baa41093432fd8163014b060f91635b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed890eecd65e943de917a462528a3e6a",
+    "url": "/copy-checker/index.html"
+  },
+  {
+    "revision": "881d5bda3f55e5139866",
+    "url": "/copy-checker/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "88eec22beb271c14fef0",
+    "url": "/copy-checker/static/css/main.e32edbe4.chunk.css"
+  },
+  {
+    "revision": "881d5bda3f55e5139866",
+    "url": "/copy-checker/static/js/2.1f476a03.chunk.js"
+  },
+  {
+    "revision": "88eec22beb271c14fef0",
+    "url": "/copy-checker/static/js/main.ffb2a937.chunk.js"
+  },
+  {
+    "revision": "65c3553ff8541f67d54c",
+    "url": "/copy-checker/static/js/runtime~main.d2a7b355.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/copy-checker/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/copy-checker/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/copy-checker/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/copy-checker/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  }
+]);
