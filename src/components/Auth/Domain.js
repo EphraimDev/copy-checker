931a163f47d6c84a962503f8ce6a293aad2a2145
Ephraim <ephraim.aigbefo@outlook.com>
2019-05-30 03:09:34 +0100

    update domain

--- src/components/Auth/Domain.js
+++ src/components/Auth/Domain.js
@@ -1,3 +1,3 @@
 //export const domain = 'https://ephaig-web.herokuapp.com/api/v1';
 
-export const domain = 'http://localhost:9999/api/v1';
+export const domain = 'https://dragon-legend-5.herokuapp.com/api/v1';
